c1e304bfa217ad9221e7d1ad83c0769ca425de27
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-12-06 14:27:24 +0300

    12/2022

---
+++ KeyPerFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(),"Files");
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddKeyPerFile(directoryPath:path,true)
+            .Build();
+
+        var dbName = configuration["DatabaseName"];
+        Console.WriteLine(dbName);
+    }
+}
